24c6be75e67abf941a69f1ab6320bb66222e0369
Demo User <demouser@MacBook-Pro.local>
2017-07-14 07:51:07 -0700

    kdn

--- summer.js
+++ summer.js
@@ -1,20 +1,3 @@
-function usercomment() {
-   var comment = $('#comment').val();
-
-   $("#top_comment").text(  comment );
-}
-function handleKey(event)
-{
-  if (event.keyCode == 13)
-  {
-    usercomment();
-  }
-}
-function setup() {
-   $("#post_button").click(usercomment);
-$('#comment').on('keypress',handleKey);
-}
-
 
 $('img').mouseover(function(){
     var newSrc = $(this).attr("src").replace("giphysum.gif", "sushine.jpg");
